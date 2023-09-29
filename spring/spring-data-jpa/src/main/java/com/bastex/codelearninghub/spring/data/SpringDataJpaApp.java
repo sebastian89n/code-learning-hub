8a34af92d31e2ff52d46bcd267b35e5f35ff8f57
Sebastian Nowak <sebastian89n@gmail.com>
2023-09-29 08:59:14 +0200

    Add Spring Data Jpa module

---
+++ spring/spring-data-jpa/src/main/java/com/bastex/codelearninghub/spring/data/SpringDataJpaApp.java
@@ -0,0 +1,11 @@
+package com.bastex.codelearninghub.spring.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaApp {
+    public static void main(final String[] args) {
+        SpringApplication.run(SpringDataJpaApp.class, args);
+    }
+}
