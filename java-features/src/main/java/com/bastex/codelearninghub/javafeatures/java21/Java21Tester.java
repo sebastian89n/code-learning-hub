c6afe2f0c09dac5412ffaf5e799843a9ec82f23d
Giuseppe Miscione <giuseppe.miscione@smartkyc.com>
2023-09-29 11:22:03 +0200

    More examples for java 21
    1) sealed hierarchy interfaced with jackson
    2) pattern matching + compile time check that all actions are handled
    3) examples of usages of new methods from SequencedCollection

--- java-features/src/main/java/com/bastex/codelearninghub/javafeatures/java21/Java21Tester.java
+++ java-features/src/main/java/com/bastex/codelearninghub/javafeatures/java21/Java21Tester.java
@@ -1,8 +1,13 @@
 package com.bastex.codelearninghub.javafeatures.java21;
 
+import com.bastex.codelearninghub.javafeatures.java21.Action.Add;
+import com.bastex.codelearninghub.javafeatures.java21.Action.Remove;
 import com.bastex.codelearninghub.javafeatures.java21.ListUtils.ListPosition;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -11,6 +16,7 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class Java21Tester
 {
+	@SneakyThrows
 	public static void testJava21Features()
 	{
 		List<Person> people = new ArrayList<>();
@@ -23,6 +29,72 @@ public final class Java21Tester
 		updateList(people, new Person(3, "Dwayne", "Johnson"));
 
 		people.forEach(System.out::println);
+
+		var mapper = new ObjectMapper();
+		final List<Action> actions = mapper.readValue(readActions(), new TypeReference<>() {});
+		List<String> values = executeActions(actions);
+		System.out.println("Values after executing the actions:");
+		values.forEach(System.out::println);
+	}
+
+	private static String readActions()
+	{
+		// simulate the retrieval of actions as a json string
+		return """
+				[
+				{
+					"type": "addLast",
+					"value": "last"
+				},
+				{
+					"type": "addFirst",
+					"value": "first"
+				},
+				{
+					"type": "addAt",
+					"position": 1,
+					"value": "at"
+				},
+				{
+					"type": "removeFrom",
+					"position": 2
+				}
+				]""";
+	}
+
+	private static List<String> executeActions(List<Action> actions)
+	{
+		List<String> values = new ArrayList<>();
+
+		actions.forEach(action -> {
+			switch (action) {
+				case Add add -> handleAdd(add, values);
+				case Remove remove -> handleRemove(remove, values);
+				case Action.Set(int position, String value) -> values.set(position, value);
+			}
+		});
+
+		return values;
+	}
+
+	private static void handleAdd(Add action, List<String> values)
+	{
+		switch (action) {
+			case Add.Last(String value) -> values.addLast(value); // new method in sequenced collections, equivalent to .add
+			case Add.First(String value) -> values.addFirst(value); // sequenced collections to the rescue!
+			case Add.At(int position, String value) -> values.add(position, value);
+		}
+	}
+
+	private static void handleRemove(Remove action, List<String> values)
+	{
+		switch (action) {
+			// with preview features we can replace "ignored" with "_" so that no variable is bound in the
+			// first 2 branches of this switch
+			case Remove.Last ignored -> values.removeLast(); // sequenced collections to the rescue!
+			case Remove.First ignored -> values.removeFirst(); // sequenced collections to the rescue!
+			case Remove.From(int position) -> values.remove(position);
+		}
 	}
 
 	private static void updateList(List<Person> people, Person target1)
