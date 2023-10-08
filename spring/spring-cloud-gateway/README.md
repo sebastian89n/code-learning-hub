7459c3bd953490acd8c347453d3e483a0db9fc05
Sebastian Nowak <sebastian89n@gmail.com>
2023-10-08 22:53:00 +0200

    Add Spring Cloud Gateway module #26

---
+++ spring/spring-cloud-gateway/README.md
@@ -0,0 +1,10 @@
+# spring-cloud-gateway
+
+Basic example of Spring Cloud Gateway usage with yaml mapping.
+
+Examples uses forwarded requests to https://httpbin.org/ to get some sample responses.
+
+Curl examples are stored in the comments in the application.yml.
+
+Same and more complex configuration can be done in Java code including custom filters etc.
+
---
+++ spring/spring-cloud-gateway/src/main/java/com/bastex/codelearninghub/spring/gateway/SpringCloudGatewayApp.java
@@ -0,0 +1,11 @@
+package com.bastex.codelearninghub.spring.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayApp {
+    public static void main(final String[] args) {
+        SpringApplication.run(SpringCloudGatewayApp.class);
+    }
+}
