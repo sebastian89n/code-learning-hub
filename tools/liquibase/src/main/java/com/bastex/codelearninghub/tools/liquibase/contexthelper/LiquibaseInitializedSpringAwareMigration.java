9ebc22c799f479be1339dc7257943aa5e4671795
Sebastian Nowak <sebastian89n@gmail.com>
2023-10-21 22:45:03 +0200

    Extract bom for base projects #70

--- tools/liquibase/src/main/java/com/bastex/codelearninghub/tools/liquibase/contexthelper/LiquibaseInitializedSpringAwareMigration.java
+++ tools/liquibase/src/main/java/com/bastex/codelearninghub/tools/liquibase/contexthelper/LiquibaseInitializedSpringAwareMigration.java
@@ -2,7 +2,7 @@ package com.bastex.codelearninghub.tools.liquibase.contexthelper;
 
 import liquibase.change.custom.CustomTaskChange;
 
-public interface LiquibaseInitializedSpringMigration extends CustomTaskChange {
+public interface LiquibaseInitializedSpringAwareMigration extends CustomTaskChange {
     @Override
     default void setUp() {
         AppContextHolder.INSTANCE.registerAsSpringBean(this);
