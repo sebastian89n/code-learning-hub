8bb04ae355ab5231c45075ffd1563faf4a54e90d
Sebastian Nowak <sebastian89n@gmail.com>
2023-12-05 08:13:10 +0100

    Refactor profiles module #96

--- spring/spring-context/spring-context-profiles/src/main/java/com/bastex/codelearninghub/spring/context/profiles/services/I18nEnServiceImpl.java
+++ spring/spring-context/spring-context-profiles/src/main/java/com/bastex/codelearninghub/spring/context/profiles/services/I18nEnServiceImpl.java
@@ -1,7 +1,12 @@
 package com.bastex.codelearninghub.spring.context.profiles.services;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
 import java.util.Locale;
 
+@Service("i18nEnService")
+@Profile({"default", "en"})
 public class I18nEnServiceImpl implements I18nService {
     @Override
     public String getGreeting() {
